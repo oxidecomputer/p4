49b0d026a4064ce70cfeccce9f89e1bab6986386
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2022-08-30 09:37:35 -0700

    fix p4 build dependencies

---
+++ lang/prog/sidecar-lite/build.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let src = [
+        "../../../test/src/p4/sidecar-lite.p4",
+    ];
+    for x in src {
+        println!("cargo:rerun-if-changed={}", x);
+    }
+}
--- test/build.rs
+++ test/build.rs
@@ -1,12 +1,12 @@
 fn main() {
     let src = [
+        "../p4/examples/codegen/router.p4",
+        "../p4/examples/codegen/hub.p4",
+        "src/p4/dynamic_router.p4",
+        "src/p4/dynamic_router_noaddr.p4",
+        "src/p4/dynamic_router_noaddr_nbr.p4",
         "src/p4/router.p4",
-        "src/p4/hub.p4",
-        "test/src/p4/dynamic_router.p4",
-        "test/src/p4/dynamic_router_noaddr.p4",
-        "test/src/p4/dynamic_router_noaddr_nbr.p4",
-        "test/src/p4/router.p4",
-        "test/src/p4/sidecar-lite.p4",
+        "src/p4/sidecar-lite.p4",
     ];
     for x in src {
         println!("cargo:rerun-if-changed={}", x);
