86a6a9439a3021aab9116feacc6b8605a977bf19
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2022-08-30 19:51:53 -0700

    fmt

--- lang/prog/sidecar-lite/build.rs
+++ lang/prog/sidecar-lite/build.rs
@@ -1,7 +1,5 @@
 fn main() {
-    let src = [
-        "../../../test/src/p4/sidecar-lite.p4",
-    ];
+    let src = ["../../../test/src/p4/sidecar-lite.p4"];
     for x in src {
         println!("cargo:rerun-if-changed={}", x);
     }
