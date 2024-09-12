e497bca06ed34e9a45528687a7cb79211cc13d9d
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2024-09-12 00:01:05 -0700

    reticulating htq codegen

---
+++ codegen/htq/src/statement.rs
@@ -0,0 +1,19 @@
+// Copyright 2024 Oxide Computer Company
+
+use crate::error::CodegenError;
+
+pub(crate) fn emit_statement(
+    stmt: &p4::ast::Statement,
+) -> Result<Vec<htq::ast::Statement>, CodegenError> {
+    use p4::ast::Statement as S;
+    match stmt {
+        S::Empty => Ok(Vec::new()),
+        S::Assignment(_lval, _expr) => todo!(),
+        S::Call(_call) => todo!(),
+        S::If(_if_block) => todo!(),
+        S::Variable(_v) => todo!(),
+        S::Constant(_c) => todo!(),
+        S::Transition(_t) => todo!(),
+        S::Return(_r) => todo!(),
+    }
+}
