b26f4b8f9e4bf67082f3e0a2d1c3c510705ae8ef
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2024-09-12 00:01:05 -0700

    reticulating htq codegen

---
+++ codegen/htq/src/expression.rs
@@ -0,0 +1,8 @@
+// Copyright 2024 Oxide Computer Company
+use crate::error::CodegenError;
+
+pub(crate) fn _emit_expression(
+    _stmt: &p4::ast::Expression,
+) -> Result<Vec<htq::ast::Statement>, CodegenError> {
+    todo!()
+}
