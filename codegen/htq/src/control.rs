bad3ad0db1559e7048455084335e0a59ed572131
Ryan Goodfellow <ryan.goodfellow@oxide.computer>
2024-09-19 00:39:36 -0700

    reticulating splines

--- codegen/htq/src/control.rs
+++ codegen/htq/src/control.rs
@@ -32,29 +32,33 @@ fn emit_control(
     afa: &mut AsyncFlagAllocator,
 ) -> Result<Vec<htq::ast::Function>, CodegenError> {
     let mut result = Vec::default();
+    let mut psub = HashMap::<ControlParameter, Vec<Register>>::default();
 
     let mut parameters = Vec::new();
-    let mut return_signature = Vec::new();
+    let mut apply_return_signature = Vec::new();
+    let mut action_return_signature = Vec::new();
     for x in &control.parameters {
         let typ = p4_type_to_htq_type(&x.ty)?;
         if x.direction.is_out() {
             // TODO the special case nature of lookup results is quite
             // unfortunate
             if x.ty.is_lookup_result() {
-                return_signature.push(htq::ast::Type::Bool); //hit
-                return_signature.push(htq::ast::Type::Unsigned(16)); //variant
+                apply_return_signature.push(htq::ast::Type::Bool); //hit
+                apply_return_signature.push(htq::ast::Type::Unsigned(16)); //variant
 
                 let args_size = control
                     .resolve_lookup_result_args_size(&x.name, ast)
                     .ok_or(CodegenError::LookupResultArgSize(x.clone()))?;
 
-                return_signature.push(htq::ast::Type::Unsigned(args_size));
+                apply_return_signature
+                    .push(htq::ast::Type::Unsigned(args_size));
 
                 if x.ty.is_sync() {
-                    return_signature.push(htq::ast::Type::Unsigned(128)); //async flag
+                    apply_return_signature.push(htq::ast::Type::Unsigned(128)); //async flag
                 }
             } else {
-                return_signature.push(typ.clone());
+                apply_return_signature.push(typ.clone());
+                action_return_signature.push(typ.clone());
             }
         }
         let p = htq::ast::Parameter {
@@ -69,8 +73,9 @@ fn emit_control(
         hlir,
         control,
         &parameters,
-        &return_signature,
+        &apply_return_signature,
         afa,
+        &mut psub,
     )?);
 
     for action in &control.actions {
@@ -80,8 +85,9 @@ fn emit_control(
             control,
             action,
             &parameters,
-            &return_signature,
+            &action_return_signature,
             afa,
+            &mut psub,
         )?);
     }
     Ok(result)
@@ -94,11 +100,11 @@ fn emit_control_apply(
     parameters: &[(Parameter, p4::ast::ControlParameter)],
     return_signature: &[htq::ast::Type],
     afa: &mut AsyncFlagAllocator,
+    psub: &mut HashMap<ControlParameter, Vec<Register>>,
 ) -> Result<htq::ast::Function, CodegenError> {
     let mut ra = RegisterAllocator::default();
     let mut names = control.names();
     let mut statements = Vec::default();
-    let mut psub = HashMap::<ControlParameter, Vec<Register>>::default();
 
     for (p, p4p) in parameters {
         if psub.get(p4p).is_some() {
@@ -117,7 +123,7 @@ fn emit_control_apply(
                 &mut names,
                 &mut ra,
                 afa,
-                &mut psub,
+                psub,
             )?
             .into_iter(),
         )
@@ -129,10 +135,15 @@ fn emit_control_apply(
     for (p, p4p) in parameters {
         if p4p.direction.is_out() {
             if let Some(substituted) = psub.get(p4p) {
-                return_registers.extend(substituted.clone().into_iter());
+                return_registers.extend(
+                    substituted
+                        .clone()
+                        .into_iter()
+                        .map(|x| ra.get_reg(&x).unwrap()),
+                );
             } else {
                 signature.push(p.clone());
-                return_registers.push(p.reg.clone());
+                return_registers.push(ra.get_reg(&p.reg).unwrap());
             }
         }
     }
@@ -158,23 +169,31 @@ fn emit_control_action(
     parameters: &[(Parameter, p4::ast::ControlParameter)],
     return_signature: &[htq::ast::Type],
     afa: &mut AsyncFlagAllocator,
+    psub: &mut HashMap<ControlParameter, Vec<Register>>,
 ) -> Result<htq::ast::Function, CodegenError> {
     let mut ra = RegisterAllocator::default();
     let mut names = control.names();
     let mut statements = Vec::default();
-    let mut parameters: Vec<Parameter> =
-        parameters.to_owned().into_iter().map(|x| x.0).collect();
-    let mut psub = HashMap::<ControlParameter, Vec<Register>>::default();
+    let parameters: Vec<(Parameter, p4::ast::ControlParameter)> = parameters
+        .iter()
+        .filter(|(_, p4p)| !p4p.ty.is_lookup_result())
+        .cloned()
+        .collect();
+    let mut action_parameters: Vec<Parameter> =
+        parameters.iter().cloned().map(|x| x.0).collect();
 
     for x in &action.parameters {
         let p = htq::ast::Parameter {
             reg: htq::ast::Register::new(x.name.as_str()),
             typ: p4_type_to_htq_type(&x.ty)?,
         };
-        parameters.push(p);
+        action_parameters.push(p);
     }
 
-    for p in &parameters {
+    for (p, p4p) in &parameters {
+        if psub.get(p4p).is_some() {
+            continue;
+        }
         ra.alloc(&p.reg.0);
     }
 
@@ -188,15 +207,44 @@ fn emit_control_action(
                 &mut names,
                 &mut ra,
                 afa,
-                &mut psub,
+                psub,
             )?
             .into_iter(),
         )
     }
 
+    let mut signature = Vec::new();
+    let mut return_registers = Vec::new();
+
+    for (p, p4p) in &parameters {
+        if p4p.direction.is_out() {
+            if let Some(substituted) = psub.get(p4p) {
+                for x in substituted {
+                    if let Some(r) = ra.get_reg(x) {
+                        return_registers.push(r.clone())
+                    }
+                }
+                /*
+                return_registers.extend(
+                    substituted
+                        .clone()
+                        .into_iter()
+                        .map(|x| ra.get_reg(&x).unwrap()),
+                );
+                */
+            } else {
+                signature.push(p.clone());
+                return_registers.push(ra.get_reg(&p.reg).unwrap());
+            }
+        }
+    }
+    statements.push(htq::ast::Statement::Return(htq::ast::Return {
+        registers: return_registers,
+    }));
+
     let f = htq::ast::Function {
         name: format!("{}_{}", control.name, action.name),
-        parameters,
+        parameters: action_parameters,
         statements,
         return_signature: return_signature.to_vec(),
     };
