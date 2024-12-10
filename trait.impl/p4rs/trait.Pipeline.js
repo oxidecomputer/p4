30347a705c095ebebe106f19427cc167504b275b
GitHub Action <action@github.com>
2024-12-10 16:14:48 +0000

    deploy

---
+++ trait.impl/p4rs/trait.Pipeline.js
@@ -0,0 +1,5 @@
+(function() {var implementors = {
+"hello_world":[["impl Pipeline for <a class=\"struct\" href=\"hello_world/struct.main_pipeline.html\" title=\"struct hello_world::main_pipeline\">main_pipeline</a>"]],
+"sidecar_lite":[["impl Pipeline for <a class=\"struct\" href=\"sidecar_lite/struct.main_pipeline.html\" title=\"struct sidecar_lite::main_pipeline\">main_pipeline</a>"]],
+"vlan_switch":[["impl Pipeline for <a class=\"struct\" href=\"vlan_switch/struct.main_pipeline.html\" title=\"struct vlan_switch::main_pipeline\">main_pipeline</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
