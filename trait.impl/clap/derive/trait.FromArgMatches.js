c673ed174eea0df3e948c516d489e6c505bf2cb8
GitHub Action <action@github.com>
2025-01-28 17:59:00 +0000

    deploy

---
+++ trait.impl/clap/derive/trait.FromArgMatches.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"x4c":[["impl FromArgMatches for <a class=\"struct\" href=\"x4c/struct.Opts.html\" title=\"struct x4c::Opts\">Opts</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
