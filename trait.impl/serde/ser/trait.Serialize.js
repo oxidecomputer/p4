93ee4b76b163432ea9272be6af424af9958c1f44
GitHub Action <action@github.com>
2025-03-28 17:35:03 +0000

    deploy

---
+++ trait.impl/serde/ser/trait.Serialize.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["p4rs",[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"p4rs/table/enum.Key.html\" title=\"enum p4rs::table::Key\">Key</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"p4rs/table/enum.Ternary.html\" title=\"enum p4rs::table::Ternary\">Ternary</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"p4rs/struct.TableEntry.html\" title=\"struct p4rs::TableEntry\">TableEntry</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"p4rs/table/struct.BigUintKey.html\" title=\"struct p4rs::table::BigUintKey\">BigUintKey</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"p4rs/table/struct.Prefix.html\" title=\"struct p4rs::table::Prefix\">Prefix</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1308]}
