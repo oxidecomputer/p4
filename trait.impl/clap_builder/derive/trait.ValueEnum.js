8bf77eb45a377315b253b7fe1c73c6333ff650fe
GitHub Action <action@github.com>
2025-07-10 14:27:57 +0000

    deploy

---
+++ trait.impl/clap_builder/derive/trait.ValueEnum.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["x4c",[["impl ValueEnum for <a class=\"enum\" href=\"x4c/enum.Target.html\" title=\"enum x4c::Target\">Target</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[118]}
