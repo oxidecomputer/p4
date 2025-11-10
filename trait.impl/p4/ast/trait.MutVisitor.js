a616995b2ab62a84f3a1c51d953b3173fa25cf2b
GitHub Action <action@github.com>
2025-11-10 13:32:45 +0000

    deploy

---
+++ trait.impl/p4/ast/trait.MutVisitor.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["p4_rust",[["impl <a class=\"trait\" href=\"p4/ast/trait.MutVisitor.html\" title=\"trait p4::ast::MutVisitor\">MutVisitor</a> for <a class=\"struct\" href=\"p4_rust/struct.Sanitizer.html\" title=\"struct p4_rust::Sanitizer\">Sanitizer</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[243]}
