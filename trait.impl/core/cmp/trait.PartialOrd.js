875ffc64c4957843932cf302139f434b8da74930
GitHub Action <action@github.com>
2026-01-07 05:53:18 +0000

    deploy

---
+++ trait.impl/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["p4",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"enum\" href=\"p4/lexer/enum.Kind.html\" title=\"enum p4::lexer::Kind\">Kind</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"p4/ast/struct.Lvalue.html\" title=\"struct p4::ast::Lvalue\">Lvalue</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"p4/lexer/struct.Token.html\" title=\"struct p4::lexer::Token\">Token</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[772]}
